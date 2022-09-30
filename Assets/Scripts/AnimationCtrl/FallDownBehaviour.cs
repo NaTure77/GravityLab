d26a06e1685a8a2b99cd9e42730fb9d6f0dd77b2
NaTure77 <xnaudrornfl@gmail.com>
2022-09-30 23:39:40 +0900

    Upload whole project

---
+++ Assets/Scripts/AnimationCtrl/FallDownBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDownBehaviour : StateMachineBehaviour
+{
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        {
+            Debug.Log("successLanding: " + StateManager.successLanding);
+            Debug.Log("isGrounded: " + StateManager.isGrounded);
+            Debug.Log("isColliderGrounded: " + StateManager.isColliderGrounded);
+        }
+        StateManager.successLanding = false;
+        StageUI.instance.SetFailedMessage();
+        StageUI.instance.ShowSecMessage();
+    }
+}
